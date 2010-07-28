c8985c6662a63caa35042c2f0cbd4e260c0dc4e3
Dan Hansen <dan@planettelex.net>
2010-07-27 22:18:49 -0600

    initial import

---
+++ src/main/java/com/planettelex/examples/MavenAndroid.java
@@ -0,0 +1,13 @@
+package com.planettelex.examples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MavenAndroid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
